495c611be2e91cc6cfe4ce94983183b44e5330d3
AsjadSiddiqui <asjadsidd@outlook.com>
2024-06-08 23:28:59 +0500

    null safety support

--- example/tello_example.dart
+++ example/tello_example.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
 import 'dart:io';
+
 import 'package:tello/tello.dart';
 
 main() async {
   // Connect to Tello when machine WiFi is already connected to the drone
-	var tello = new ConnectTello();
+  var tello = new ConnectTello();
 
-  while(true) {
+  while (true) {
     stdout.write('Write a command: ');
-    String command = stdin.readLineSync();
+    String? command = stdin.readLineSync();
+    if (command == null) continue;
     tello.sendCommand(command);
     await Future.delayed(Duration(milliseconds: 100));
   }
--- lib/tello.dart
+++ lib/tello.dart
@@ -4,54 +4,52 @@ import 'dart:io';
 
 // Class Singleton
 class ConnectTello {
-	static ConnectTello _inicializeTello;
-  
+  static ConnectTello? _inicializeTello;
+
   // StreamController used to manage tello commands
-  StreamController<String> _controller;
+  late StreamController<String> _controller;
 
   // Default IP and Port to send commands e recieve responses
-	var _addressesDrone = InternetAddress('192.168.10.1');
+  var _addressesDrone = InternetAddress('192.168.10.1');
   int _portDrone = 8889;
 
   // Machine IP
-	static var _addressMachine = InternetAddress('0.0.0.0');
+  static var _addressMachine = InternetAddress('0.0.0.0');
 
   // Inicialization of Constructor with default machine port
-	factory ConnectTello({int port = 9000}) {
-
+  factory ConnectTello({int port = 9000}) {
     // If object was not before, connect to Tello
-		if (_inicializeTello == null)
-			_inicializeTello = new ConnectTello._connection(_addressMachine, port);
+    if (_inicializeTello == null) _inicializeTello = new ConnectTello._connection(_addressMachine, port);
 
-		return _inicializeTello;
-	}
+    return _inicializeTello!;
+  }
 
-	ConnectTello._connection(InternetAddress localIP, int port) {
+  ConnectTello._connection(InternetAddress localIP, int port) {
     // Inicialization of StreamController and Connection to socket
     _controller = StreamController<String>();
-		connectSocket(localIP, port);
-	}
+    connectSocket(localIP, port);
+  }
 
-	connectSocket(InternetAddress localIP, int port) {
+  connectSocket(InternetAddress localIP, int port) {
     // Connecting to tello, listening to Tello response messages and sending commands when controller recieve a command
-		RawDatagramSocket.bind(localIP, port)
-    .then((socket) {
+    RawDatagramSocket.bind(localIP, port).then((socket) {
       socket.forEach((event) {
         if (event == RawSocketEvent.read) {
-          Datagram dg = socket.receive();
+          Datagram? dg = socket.receive();
+          if (dg == null) return;
           dg.data.forEach((x) => stdout.write(String.fromCharCode(x)));
           stdout.write('\n');
         }
-    	});
-        
-      _controller.stream.listen((event) { 
+      });
+
+      _controller.stream.listen((event) {
         socket.send(Utf8Codec().encode(event), _addressesDrone, _portDrone);
       });
     });
-	}
+  }
 
-	void sendCommand(String data) {
+  void sendCommand(String data) {
     // Sends a command event to controller
     _controller.sink.add(data);
-	}
+  }
 }
